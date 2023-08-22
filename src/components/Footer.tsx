571fc6a5abedacd7fe3b219a504473d555383cbe
Alexey Budkin <anibaka9@gmail.com>
2023-08-23 01:02:13 +0600

    created simple todo mockup

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="sticky top-[100vh] footer footer-center p-4 bg-base-300 text-base-content">
+      <div>
+        <p>Copyright © 2023</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/TopNav.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+
+export default function TopNav() {
+  return (
+    <div className="navbar bg-base-00">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">Todo</a>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+        </div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <Image
+                alt="user pic"
+                width={40}
+                height={40}
+                src="/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Profile</a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
